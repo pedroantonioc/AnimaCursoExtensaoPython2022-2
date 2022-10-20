9dc61c7229ebcf2e4b7ab993b861e487d52ece9f
Pedro Carvalho <112666350+pedroantonioc@users.noreply.github.com>
2022-10-20 00:54:20 +0000

    Aprendi a criar variáveis e exibi-las na tela

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
 # Meu primeiro projeto Python!!!
-#
+
 # print() = comando de saída
 print("Hello world!")
 
-      
+# quando quiser guardar uma String! (frase) 
+nome = "Pedro Carvalho"
+
+# quando quiser guardar um número inteiro
+idade = 22
+
+#exibir o meu nome (que está dentro da variável nome)
+print(nome)
+
+#exibir minha idade (que está na variável idade)
+print(idade)
+
+
+
 '''
 comentário em bloco
-
 '''
