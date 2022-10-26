e2b9572c8911dd400770b3f450e2c7e72c9cd719
Pedro Carvalho <112666350+pedroantonioc@users.noreply.github.com>
2022-10-26 23:13:40 +0000

    aprendi comando input() e entendi quando declarar o tipo de variável

---
+++ Aula1-2022-10-19.py
@@ -0,0 +1,41 @@
+# Meu primeiro projeto Python!!!
+
+# print() = comando de saída
+print("Hello world!")
+
+#quando quiser fazer comentários usar # ou '''  ''' para comentários em bloco 
+'''
+comentário em bloco
+'''
+
+# quando quiser guardar uma String! (frase) 
+nome = "Pedro Carvalho"
+
+# quando quiser guardar um número inteiro
+idade = 22
+
+#exibir o meu nome (que está dentro da variável nome)
+print(nome)
+
+#exibir minha idade (que está na variável idade)
+print(idade)
+
+
+#exibir a frase "minha idade é" completando com o conteúdo da variável idade.
+print("minha idade é: "+str(idade))
+print(f"minha idade é: {idade}\n")
+print("minha idade é {}".format(idade))
+
+'''anotações
+(o str é um comando de conversão para concatenar tipos diferentes de variáveis)
+(o f e {} são comandos para concatenar)
+(\n é para pular linha)
+(.format é outra forma de escrever o comando)
+'''
+#quando quiser exibir "meu nome é... e tenho... anos" trocando pelas variáveis nome e idade
+print("meu nome é {} e tenho {} anos".format(nome, idade))
+
+''' anotações
+(quando quiser imprimir várias variáveis com o format, abrir chaves e depois colocar as variáveis no comando .format na ordem que quero exibilas)
+'''
+
--- main.py
+++ main.py
@@ -1,40 +1,16 @@
-# Meu primeiro projeto Python!!!
+#comando input(): quero permitir que o usuário digite algo...
+nome = input("digite seu nome: ")
 
-# print() = comando de saída
-print("Hello world!")
+#comando de saída... exibe na tela
+print(f"Boa noite, seu nome é {nome}")
 
-#quando quiser fazer comentários usar # ou '''  ''' para comentários em bloco 
-'''
-comentário em bloco
-'''
+#exiba "Sua idade é..."
+#necessário declarar o tipo de variável pois estava armazenando direto como string
+idade = int(input("digite sua idade:"))
+print(f"Sua idade é {idade} anos")
 
-# quando quiser guardar uma String! (frase) 
-nome = "Pedro Carvalho"
+#exiba com format
+print("Sua idade é {}".format(idade))
 
-# quando quiser guardar um número inteiro
-idade = 22
-
-#exibir o meu nome (que está dentro da variável nome)
-print(nome)
-
-#exibir minha idade (que está na variável idade)
-print(idade)
-
-
-#exibir a frase "minha idade é" completando com o conteúdo da variável idade.
-print("minha idade é: "+str(idade))
-print(f"minha idade é: {idade}\n")
-print("minha idade é {}".format(idade))
-
-'''anotações
-(o str é um comando de conversão para concatenar tipos diferentes de variáveis)
-(o f e {} são comandos para concatenar)
-(\n é para pular linha)
-(.format é outra forma de escrever o comando)
-'''
-#quando quiser exibir "meu nome é... e tenho... anos" trocando pelas variáveis nome e idade
-print("meu nome é {} e tenho {} anos".format(nome, idade))
-
-''' anotações
-(quando quiser imprimir várias variáveis com o format, abrir chaves e depois colocar as variáveis no comando .format na ordem que quero exibilas)
-'''
+dobro = idade * 2
+print("o dobro da sua idade é {}".format(dobro)) 
