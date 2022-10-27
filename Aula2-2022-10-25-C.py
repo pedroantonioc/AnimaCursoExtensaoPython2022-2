903cd79ae3c867d66ff54a9149124baead9baf73
Pedro Carvalho <112666350+pedroantonioc@users.noreply.github.com>
2022-10-27 01:23:07 +0000

    exemplo de while

---
+++ Aula2-2022-10-25-C.py
@@ -0,0 +1,23 @@
+#Exemplo de laço
+#é um if que se repete
+
+# se eu quisesse exibir números de 1 a 10
+print("aqui mostramos de maneira rústica")
+print("1")
+print("2")
+print("3")
+print("4")
+print("5")
+print("6")
+print("7")
+print("8")
+print("9")
+print("10")
+
+# com o while
+print("aqui é do jeito certo")
+num = 1
+while(num<=10):
+  print(num)
+  num = num + 1
+  
